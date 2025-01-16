d90f25367d22843ecdc4157440071671842b77c6
Garrick Wong <inquisitor8986@gmail.com>
2025-01-16 16:28:04 -0700

    Base files

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String[] cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin",
+                "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        view.findViewById(R.id.add_city_button).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+
+                    public void onClick(View view) {
+                        NavHostFragement
+                    }
+                }
+        );
+
+        EdgeToEdge.enable(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
